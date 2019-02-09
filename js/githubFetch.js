4918e6a73e8e71911781568d1b7a4f3ba02d37c3
Martin Dimitrov <martin3dimitrov@gmail.com>
2019-02-08 21:00:21 -0500

    update look and feel

--- js/githubFetch.js
+++ js/githubFetch.js
@@ -9,6 +9,7 @@ function main(){
     console.log("Repositories", martRepos.repositories);
     var updatedStr = "";
     var test;
+    var projects = 0;
     for (i = 0; i<martRepos.repositories.length; i++){
       test = $.ajax(
         {
@@ -23,13 +24,25 @@ function main(){
                          },
             async: false
         });
+      if (projects < 2){
+        test.statusText = "success";
+      }
       if (test.statusText == "success"){
-        updatedStr += "<div class = \"column\"><h1>" + martRepos.repositories[i].name + "</h1><br><p>" + test.statusText + "<br><br></p><a href=\"https://github.com/martindmtrv/"+ martRepos.repositories[i].name  + "\">View project on GitHub</a></div>"
+        updatedStr += "<div class = \"column\"><h4>" + martRepos.repositories[i].name + "</h4><br><p>" + test.statusText + "<br><br></p><a href=\"https://github.com/martindmtrv/"+ martRepos.repositories[i].name  + "\">View project on GitHub</a></div>";
+        projects++;
       }
       else{
-        document.getElementById("other").innerHTML += "<p>" + martRepos.repositories[i].name + "<br><br></p>"
+        document.getElementById("other").innerHTML += "<p>" + martRepos.repositories[i].name + "<br><br></p>";
       }
     }
-    document.getElementById("response").innerHTML = updatedStr
+    if (projects >= 3){
+      document.getElementById("response").innerHTML = updatedStr;
+    } else{
+      for (var x = projects; x < 3; x++){
+        updatedStr += "<div class = \"column\"></div>"
+      }
+      document.getElementById("response").innerHTML = updatedStr;
+    }
+    
   })
 }
