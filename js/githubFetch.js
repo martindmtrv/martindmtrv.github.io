07e3149a78d61ac046a73924f68d6c8ba8c3137f
Martin Dimitrov <martin3dimitrov@gmail.com>
2019-03-18 16:49:25 -0400

    undo async changes

--- js/githubFetch.js
+++ js/githubFetch.js
@@ -22,35 +22,33 @@ martRepos.fetch({page:1, per_page:10, direction : "asc"},"next", function (err, 
                               console.log("404");
                             }
                         },
-          async: true,
-          callback: updateTable(this)
+          async: false
       });
-  }
-})
-
-function updateTable(test){
-  if (test.statusText == "success"){
-    description = test.responseText;
-    description = $(description).find("p");
-    if (description[0] === undefined){
-      description = "A cool project description";
+    if (test.statusText == "success"){
+      description = test.responseText;
+      description = $(description).find("p");
+      if (description[0] === undefined){
+        description = "A cool project description";
+      } else{
+        description = description[0].innerText;
+      }
+      if (martRepos.repositories[i].name != "martindmtrv.github.io" && martRepos.repositories[i].name != "spaghetti-code"){
+        updatedStr += "<div class = \"column\"><h4>" + martRepos.repositories[i].name + "</h4><p>"+ description + "</p><a href=\"https://github.com/martindmtrv/"+ martRepos.repositories[i].name  + "\">View on GitHub</a></div>";
+        projects++;
+      } 
+    }
+    else{
+      document.getElementById("other").innerHTML += "<p>" + martRepos.repositories[i].name + "<br><br></p>";
+    }
+    if (projects >= 3){
+      document.getElementById("response").innerHTML = updatedStr;
     } else{
-      description = description[0].innerText;
+      for (var x = projects; x < 3; x++){
+        updatedStr += "<div class = \"column\"></div>"
+      }
+      document.getElementById("response").innerHTML = updatedStr;
     }
-    if (martRepos.repositories[i].name != "martindmtrv.github.io" && martRepos.repositories[i].name != "spaghetti-code"){
-      updatedStr += "<div class = \"column\"><h4>" + martRepos.repositories[i].name + "</h4><p>"+ description + "</p><a href=\"https://github.com/martindmtrv/"+ martRepos.repositories[i].name  + "\">View on GitHub</a></div>";
-      projects++;
-    } 
   }
-  else{
-    document.getElementById("other").innerHTML += "<p>" + martRepos.repositories[i].name + "<br><br></p>";
-  }
-  if (projects >= 3){
-    document.getElementById("response").innerHTML = updatedStr;
-  } else{
-    for (var x = projects; x < 3; x++){
-      updatedStr += "<div class = \"column\"></div>"
-    }
-    document.getElementById("response").innerHTML = updatedStr;
-  }
-}
+  
+});
+
