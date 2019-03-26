40a5220d9cee7f3892a261db82c911c55fcb40f4
Martin Dimitrov <martin3dimitrov@gmail.com>
2019-03-26 11:10:35 -0400

    change up callbakc page

--- js/authQuest.js
+++ js/authQuest.js
@@ -14,7 +14,7 @@ function auth(code){
   console.log(code);
 
   if (typeof code == "undefined"){
-    window.open("https://martindmtrv.github.io/authRedirect.html");
+    window.open("https://login.questrade.com/oauth2/authorize?client_id=TAmdaaZ2WI35zAIAGCwQWE8uNc_k9A&response_type=code&redirect_uri=https://martindmtrv.github.io/callback.html");
   } else{
       $.get({
         url: "https://login.questrade.com/oauth2/token?client_id="+client_id+"&code="+code+"grant_type=authorization_code&redirect_uri=https://martindmtrv.github.io/callback.html",
