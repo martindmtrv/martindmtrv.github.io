65c8425ab540556c8977c87429bb75595adbd13e
Martin Dimitrov <martin3dimitrov@gmail.com>
2019-03-27 20:31:35 -0400

    fix js

--- js/reveal.js
+++ js/reveal.js
@@ -1,21 +1,18 @@
-$(function(){
-    $(".button").click(function() {    
-		
-        var $a, $p, $up;
+function reveal(){  
+    var $a, $p, $up;
 
-        $a = $('.sidebar-box .button');
-        $p  = $a.parent();
-        $up = $p.parent();
-                    
-        $up
-            .animate({
-                "max-height": 4*1000
-            });
-        
-        // fade out read-more
-        $p.fadeOut();
-        
-        // prevent jump-down
-        return false;        
-    });
-});
+    $a = $('.sidebar-box .button');
+    $p  = $a.parent();
+    $up = $p.parent();
+                
+    $up
+        .animate({
+            "max-height": 4*1000
+        });
+    
+    // fade out read-more
+    $p.fadeOut();
+    
+    // prevent jump-down
+    return false;        
+}
